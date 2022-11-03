8eb54f3f47dd88b840997d52e7af13b0d2991830
dev1 <dev1@example.com>
2022-11-03 17:10:39 +0300

    add more codes

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,5 +1,15 @@
-import { Paper } from "@mui/material";
+import {
+  Delete,
+  Edit,
+  Key,
+  Lock,
+  LockOpen,
+  RemoveRedEyeOutlined,
+} from "@mui/icons-material";
+import { Chip, Paper } from "@mui/material";
+import { Space } from "antd";
 import React, { useState } from "react";
+import ActionBtn from "../../components/ActionBtn/ActionBtn";
 import DataTableUi from "../../components/DataTableUi/DataTableUi";
 import TitleBoxUi from "../../components/TitleBoxUi/TitleBoxUi";
 import { GET_ALL_USERS } from "./graphQL";
@@ -27,15 +37,46 @@ const Users = () => {
       title: "Username",
       dataIndex: "username",
       key: "username",
-    },
-    {
-      title: "Created At",
-      dataIndex: "createdAt",
-      key: "createdAt",
+      render: (_, rec) => {
+        return (
+          <>
+            <Chip size="small" label={rec.username} />
+          </>
+        );
+      },
     },
     {
       title: "Actions",
       key: "actions",
+      render: (_, rec) => (
+        <Space>
+          <ActionBtn
+            icon={<Edit color="info" />}
+            onClickIcon={() => null}
+            title="Edit Users"
+          />
+          <ActionBtn
+            icon={<Delete color="error" />}
+            onClickIcon={() => null}
+            title="Delete Users"
+          />
+          {rec.active ? (
+            <ActionBtn
+              icon={<Lock color="error" />}
+              onClickIcon={() => null}
+              title="Activate Users"
+            />
+          ) : (
+            <ActionBtn
+              icon={<LockOpen color="success" />}
+              onClickIcon={() => null}
+              title="Block Users"
+            />
+          )}
+          <ActionBtn icon={<RemoveRedEyeOutlined />} />
+          <ActionBtn icon={<Key color="warning" />} />
+        </Space>
+      ),
     },
   ];
   return (
